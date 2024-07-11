dc90d00b9e805a67182e2650544c8f4b74584736
lok70 <lokwer321@gmail.com>
2024-07-11 12:41:22 +0300

    add

---
+++ sometasks/tasksasd/tasksasd.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "Header.h"
+
+using namespace std;
+
+int main()
+{
+	auto str = "[<(>])";
+	auto output = makeValidSequence(str);
+	cout << output << endl;
+
+
+    const int n = 50; // длина ряда
+    int total_ways = 0;
+
+	cout << Finish(50) << endl;
+}
